5c6eba27bb47497e800a64d99db76cf4e829ed05
lsswizard <lsscotter1977@aol.com>
2016-07-27 15:33:48 +0100

    More missing Voltage Control Code

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -745,7 +745,9 @@ cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
-cpufreq_freq_attr_rw(UV_mV_table);
+#ifdef CONFIG_CPU_VOLTAGE_CONTROL
+ cpufreq_freq_attr_rw(UV_mV_table);
+#endif
 
 static struct attribute *default_attrs[] = {
 	&cpuinfo_min_freq.attr,
@@ -763,7 +765,9 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
+	#ifdef CONFIG_CPU_VOLTAGE_CONTROL
 	&UV_mV_table.attr,
+	#endif
 	NULL
 };
 
