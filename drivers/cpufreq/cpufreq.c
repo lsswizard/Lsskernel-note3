3913c1593f72c901638c878ed7eb21c421e33233
lsswizard <lsscotter1977@aol.com>
2016-08-31 20:21:10 +0100

    Correct governor change on all cores

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1994,14 +1994,15 @@ static int __cpufreq_set_policy(struct cpufreq_policy *data,
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
 			CPUFREQ_NOTIFY, policy);
 
-	data->min = policy->min;
-	if (policy->cpu >= 1) {
+if (policy->cpu) {
 		cpu0_policy = __cpufreq_cpu_get(0,0);
+		data->min = cpu0_policy->min;
 		data->max = cpu0_policy->max;
 	} else {
+		data->min = policy->min;
 		data->max = policy->max;
 	}
-
+	
 	pr_debug("new min and max freqs are %u - %u kHz\n",
 					data->min, data->max);
 
@@ -2021,7 +2022,12 @@ static int __cpufreq_set_policy(struct cpufreq_policy *data,
 				__cpufreq_governor(data, CPUFREQ_GOV_STOP);
 
 			/* start new governor */
-			data->governor = policy->governor;
+			if (policy->cpu && cpu0_policy) {
+				data->governor = cpu0_policy->governor;
+			} else {
+				data->governor = policy->governor;
+			}
+
 
 			if (__cpufreq_governor(data, CPUFREQ_GOV_START)) {
 				/* new governor failed, so re-start old one */
