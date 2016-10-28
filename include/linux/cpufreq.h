858536bbf8f6474a3a60badefa60bc7ee82bfb02
mostafa-z <mostafazarghami@gmail.com>
2016-10-28 03:39:22 +0330

    Added Alucard, Darkness, Nightmare Gov's plus more fixes

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -538,6 +538,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ARTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_arteractive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_arteractive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
+extern struct cpufreq_governor cpufreq_gov_alucard;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
 #endif
 
 /*********************************************************************
